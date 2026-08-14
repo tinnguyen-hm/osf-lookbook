ae257b36fb8fe260185b5e5547be1e2037f1d103
tinnguyen-hm <tin.nguyen@hikemedical.com>
2026-08-14 03:43:05 -0500

    Create comments-config.js

---
+++ js/comments-config.js
@@ -0,0 +1,3 @@
+// Paste the Web App URL you get after deploying apps-script/Code.gs here.
+// Instructions are in README.md under "Set up commenting".
+window.COMMENTS_ENDPOINT = "https://script.google.com/macros/s/AKfycbzCyKM3LiJdgrRzKC7sKmjruwoJ_iSUqkH_l1Z6USdFtaDKarWRNUz4iSYB1l4C265J/exec";
